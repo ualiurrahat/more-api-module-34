f80c7923aeccb7235f8bcba7f9358f103ebe9451
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-02 23:36:52 +0600

    handled random user with nested api data

---
+++ js/randomUser.js
@@ -0,0 +1,34 @@
+function loadUser()
+{
+    fetch('https://randomuser.me/api/?gender=female')
+    .then(res =>res.json())
+    .then(data =>displayUser(data))
+}
+function displayUser(data)
+{
+    console.log(data.results[0]);
+    // get name and show it
+    const nameElement = document.getElementById("name");
+    nameElement.innerText = data.results[0].name.title + " " + data.results[0].first + " " + data.results[0].name.last;
+    // get gender and set it to gender
+    const genderElement = document.getElementById("gender");
+    genderElement.innerText = data.results[0].gender;
+    // get locaction city
+    const cityElement = document.getElementById("city");
+    cityElement.innerText = data.results[0].location.city;
+
+    // get street.
+    const streetElement = document.getElementById("street");
+    streetElement.innerText = data.results[0].location.street.name +" " +  data.results[0].location.street.number;
+
+    // get image
+    const imageElement = document.getElementById("image");
+   const link= data.results[0].picture.medium;
+   const imageDiv = document.createElement("div");
+   imageDiv.innerHTML = `
+   <img src = "link">
+   `;
+   imageElement.appendChild(imageDiv);
+
+}
+loadUser();
