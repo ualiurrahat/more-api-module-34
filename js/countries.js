c681feee204333101a175056bb4326de7f093b99
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-03 11:41:59 +0600

    called dynamic api, loaded dynamic data to display

--- js/countries.js
+++ js/countries.js
@@ -10,14 +10,34 @@ const displayCountries = countries => {
     //     console.log(country);
     // }
     countries.forEach(country => {
-        // console.log(country.capital);
+        // console.log(country);
         const countryDiv = document.createElement("div");
+        countryDiv.classList.add("country");
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <h4>Capital: ${country.capital ? country.capital[0]: "no capital"}</h4>
+        <button onclick = "loadCountryDetails('${country.cca2}')">Details</button>
         
         `;
         countriesContainer.appendChild(countryDiv);
     });
+}
+
+const loadCountryDetails = code =>{
+    const url = `
+    https://restcountries.com/v3.1/alpha/${code}`;
+    // console.log(url);
+    fetch(url)
+    .then(res =>res.json())
+    .then(data =>showCountryDetails(data[0]))
+}
+
+const showCountryDetails = country =>{
+    const detailContainer = document.getElementById("country-detail");
+    detailContainer.innerHTML = `
+    <h3>Name: ${country.name.common}</h3>
+    <img src="${country.flags.png}">
+    
+    `
 }
 loadCountries();
