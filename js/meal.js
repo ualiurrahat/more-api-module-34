623073eaf4e74047da09c06786496695883d241d
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-03 16:24:03 +0600

    displayed dynamic search results using bootstrap cards

--- js/meal.js
+++ js/meal.js
@@ -1,5 +1,6 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
 }
@@ -7,6 +8,8 @@ const loadMeals = () => {
 const displayMeals = (meal) => {
     // 1.get container element
     const mealContainer = document.getElementById("meal-container");
+    // resetting innertHTML to null to show new result for each query
+    mealContainer.innerHTML = "";
     meal.forEach(meal => {
         console.log(meal);
         // 2. create child element of meal container for each meal
@@ -15,9 +18,9 @@ const displayMeals = (meal) => {
         // 3. set content for child element
         mealDiv.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                 to additional content. This content is a little bit longer.</p>
         </div>
@@ -26,5 +29,16 @@ const displayMeals = (meal) => {
     mealContainer.appendChild(mealDiv);
     });
 }
+const searchMeals = () =>{
+    // take user search query from search field value
+    const searchField =document.getElementById("search-field");
+    const searchText = searchField.value;
+    console.log(searchText);
+    // set search query value to null
+    searchField.value = "";
+    // now search food according to user search text
+    loadMeals(searchText);
 
-loadMeals();
+    
+}
+loadMeals('rice');
