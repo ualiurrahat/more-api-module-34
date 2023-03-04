79ff4e3542470a334d9126dd427638924f04bb93
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-04 12:41:32 +0600

    explored async await and fetch error handling

--- js/meal.js
+++ js/meal.js
@@ -23,7 +23,7 @@ const displayMeals = (meal) => {
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                 to additional content. This content is a little bit longer.</p>
-            <button onclick = "loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+            <button onclick = "loadMealDetail2(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
             Details
             </button>
         </div>
@@ -45,19 +45,37 @@ const searchMeals = () => {
 
 }
 
-const loadMealDetail = idMeal =>{
+const loadMealDetail = idMeal => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
     fetch(url)
-    .then(res =>res.json())
-    .then(data =>displayMealDetail(data.meals[0]))
-    
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+        .catch(error => console.log(error))
+
 }
 
-const displayMealDetail = meal=>{
+// async await
+const loadMealDetail2 = async (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    //    const res = await fetch(url);
+    //    const data = await res.json();
+    //    displayMealDetail(data.meals[0]);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMealDetail(data.meals[0]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+const displayMealDetail = meal => {
     document.getElementById("mealDetailsLabel").innerText = meal.strMeal;
     const mealDetailBody = document.getElementById("mealDetailBody");
     mealDetailBody.innerHTML = `
     <img class="img-fluid" src="${meal.strMealThumb}">
     `;
 }
-loadMeals('rice');
+loadMeals('fish');
